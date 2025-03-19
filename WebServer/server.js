42a415b3ec07087080189e15661d12339d09cfec
Radi0Games <sentynelpt@gmail.com>
2025-03-19 09:20:40 +0000

    veiculo e server

--- WebServer/server.js
+++ WebServer/server.js
@@ -1,42 +1,37 @@
-const express = require('express');
-const mqtt = require('mqtt');
+const express = require("express");
+const mqtt = require("mqtt");
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-// Configuração do broker MQTT
-const brokerUrl = 'mqtt://broker.hivemq.com'; 
-const client = mqtt.connect(brokerUrl);
+// MQTT Broker Connection
+const brokerUrl = "mqtt://test.mosquitto.org"; // Change to your broker URL if needed
+const mqttClient = mqtt.connect(brokerUrl);
 
-// Conectar ao broker MQTT
-client.on('connect', () => {
-    console.log('Conectado ao broker MQTT');
-
-    
-    client.subscribe('esp32/dados', (err) => {
-        if (!err) {
-            console.log('Inscrito no tópico: esp32/dados');
-        }
-    });
+mqttClient.on("connect", () => {
+  console.log("Connected to MQTT broker");
 });
 
-client.on('message', (topic, message) => {
-    console.log(`Mensagem recebida do ESP32: ${message.toString()}`);
+// Middleware for parsing JSON requests
+app.use(express.json());
+
+// MQTT topic for ESP32
+const topic = "esp32/car/move";
+
+// API Endpoint to send movement commands
+app.post("/move", (req, res) => {
+  const { direction } = req.body;
+
+  if (!["up", "down", "left", "right"].includes(direction)) {
+    return res.status(400).json({ error: "Invalid direction" });
+  }
+
+  mqttClient.publish(topic, direction, () => {
+    console.log(`Command sent: ${direction}`);
+    res.json({ message: `Command '${direction}' sent` });
+  });
 });
 
-// Rota principal
-app.get('/', (req, res) => {
-    res.send('WebServer do Nosso Veículo!');
-});
-
-
-app.get('/comando/:msg', (req, res) => {
-    const msg = req.params.msg;
-    client.publish('esp32/comando', msg); 
-    res.send(`Comando enviado: ${msg}`);
-});
-
-// Inicia o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
---
+++ veiculo/veiculo.ino
@@ -0,0 +1,101 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "your_wifi_ssid";
+const char* password = "your_wifi_password";
+const char* mqtt_server = "test.mosquitto.org";  // Change if using another broker
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const int motor1A = 5;
+const int motor1B = 18;
+const int motor2A = 19;
+const int motor2B = 21;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  while (!client.connected()) {
+    if (client.connect("ESP32Car")) {
+      client.subscribe("esp32/car/move");
+    } else {
+      delay(1000);
+    }
+  }
+
+  pinMode(motor1A, OUTPUT);
+  pinMode(motor1B, OUTPUT);
+  pinMode(motor2A, OUTPUT);
+  pinMode(motor2B, OUTPUT);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String command = "";
+  for (int i = 0; i < length; i++) {
+    command += (char)payload[i];
+  }
+  
+  Serial.println("Command Received: " + command);
+
+  if (command == "up") {
+    moveForward();
+  } else if (command == "down") {
+    moveBackward();
+  } else if (command == "left") {
+    turnLeft();
+  } else if (command == "right") {
+    turnRight();
+  } else {
+    stopMotors();
+  }
+}
+
+void moveForward() {
+  digitalWrite(motor1A, HIGH);
+  digitalWrite(motor1B, LOW);
+  digitalWrite(motor2A, HIGH);
+  digitalWrite(motor2B, LOW);
+}
+
+void moveBackward() {
+  digitalWrite(motor1A, LOW);
+  digitalWrite(motor1B, HIGH);
+  digitalWrite(motor2A, LOW);
+  digitalWrite(motor2B, HIGH);
+}
+
+void turnLeft() {
+  digitalWrite(motor1A, LOW);
+  digitalWrite(motor1B, HIGH);
+  digitalWrite(motor2A, HIGH);
+  digitalWrite(motor2B, LOW);
+}
+
+void turnRight() {
+  digitalWrite(motor1A, HIGH);
+  digitalWrite(motor1B, LOW);
+  digitalWrite(motor2A, LOW);
+  digitalWrite(motor2B, HIGH);
+}
+
+void stopMotors() {
+  digitalWrite(motor1A, LOW);
+  digitalWrite(motor1B, LOW);
+  digitalWrite(motor2A, LOW);
+  digitalWrite(motor2B, LOW);
+}
+
+void loop() {
+  client.loop();
+}
